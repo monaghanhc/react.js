505b3f4a7ad3c95d943b8176d44e7a698a397597
monaghanhc <56236377+monaghanhc@users.noreply.github.com>
2020-08-15 13:16:19 -0400

    Adding background
    adding new background for the different types of temperature

---
+++ Final-Weather-App_Hunter-Monaghan/Mr.Weather/src/App.js
@@ -0,0 +1,93 @@
+// Weather App using react.js and node.js
+// Author: Hunter Monaghan    
+
+// input: City or Country 
+// output: temperature and forecast
+
+//     Filename: App.js
+
+'strict'
+
+import React, { useState } from 'react';
+const api = {
+  key: "c44c0cbd43b5dcb8f412b6656f8f93db",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+const [query, setQuery] = useState('')
+const [weather, setWeather] = useState({});
+
+const search = evt => {
+  if(evt.key === "Enter"){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(weather);
+        console.log(result);
+      });
+  }
+}
+
+
+
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+   "October","November", "December"];
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+   "Friday", "Saturday"];
+
+   let day = days[d.getDay()];
+   let date = d.getDate();
+   let month = months[d.getMonth()];
+   let year = d.getFullYear();
+
+   return `${day} ${date} ${month} ${year}`
+}
+
+
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app' }>
+      <main>
+    
+        <div className="search-box">
+          
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            /> 
+        </div>
+
+        {/* <h1 id="header">
+        The Daily Forcast
+      </h1> */}
+      {(typeof weather.main != "undefined") ? (
+  <div>
+      <div className="location-box">
+      <div className="location">{weather.name}, {weather.sys.country}</div>
+      <div className="date">{dateBuilder(new Date())}</div>
+    </div>
+      <div className="weather-box">
+      <div className="temp">
+       {Math.round(weather.main.temp)}°C
+     </div>
+      <div className="weather">{weather.weather[0].main}</div>
+    </div>
+  </div>
+         ) : ('')}
+      </main>
+    </div>
+  
+  );
+}
+
+
+export default App;
