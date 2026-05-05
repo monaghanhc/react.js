2b8355a57f576569a9083c90172774aa3471cfa5
Hunter Monaghan <hunter.monaghan5@gmail.com>
2026-05-05 19:56:55 -0400

    Updates

---
+++ precache-manifest.b9da6847988e9e78ac29c605615f687f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806d5573e8b3f9466c84f9b8dea9a9fc",
+    "url": "/react.js/index.html"
+  },
+  {
+    "revision": "e43b2c8ec54635cd1d83",
+    "url": "/react.js/static/css/main.0647cbab.chunk.css"
+  },
+  {
+    "revision": "f62f7b990b3c5203d016",
+    "url": "/react.js/static/js/2.e447b050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js/static/js/2.e447b050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43b2c8ec54635cd1d83",
+    "url": "/react.js/static/js/main.bd155675.chunk.js"
+  },
+  {
+    "revision": "5083bc3f0cacc337cbf1",
+    "url": "/react.js/static/js/runtime-main.38e288de.js"
+  },
+  {
+    "revision": "c4048225327a8fc0981bfa06e1d59180",
+    "url": "/react.js/static/media/coldWeather.c4048225.jpg"
+  },
+  {
+    "revision": "54626914e4d9e186a0c5b05634ea59da",
+    "url": "/react.js/static/media/niceWeather.54626914.jpg"
+  }
+]);
